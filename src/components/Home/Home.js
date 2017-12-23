0795c6572b33c3b24a879489897b3995149a55c9
loren pabst <lorenpabst@gmail.com>
2017-12-22 22:51:28 -0700

    made some changes during filming take 1

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import Pin from './Pin/Pin.js';
+import Pin from './../Pin/Pin.js';
 import './Home.css';
 
 
 class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pins:[]
+    }
+  }
+
+  componentDidMount(){
+    axios.get('/mydatabase/getpins')
+    then( response => {
+      this.setState({
+        pins: response.data.pins
+      })
+    })
+  }
 
   render() {
     return (
       <div className="home">
 
-        
+        {
+          this.state.pins.map( (item, i) => {
+            return <Pin src={item.src} caption={item.caption} imgSource={item.imgSource} />
+          })
+        }
 
       </div>
     );
--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -2,23 +2,20 @@ import React, { Component } from 'react';
 import './Pin.css';
 
 class Pin extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-        }
-
-    }
-
-    componentDidMount() {
-
-    }
-
     render() {
         return (
             <section className='pin_container'>
+
+                <div className='pin_image_wrapper'>
+                    <img className='pin_image' src={this.props.src} alt='pin pic' />
+                    <div className='pin_upload'></div>
+                    <div className='pin_save'></div>
+                    <p className='pin_img_source'>{this.props.imageSource}</p>
+                </div>
+
+                <p className='pin_caption'>
+                    {this.props.caption}
+                </p>
 
             </section>
         );
